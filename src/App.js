62c8d9f0a2c2369707ee9125e95007a61f664ede
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2023-08-13 00:54:31 +0530

    Url Shortner Front-End

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UrlShortner from "./Pages/UrlShortner";
+import Login from "./Pages/Login";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Register from "./Pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Register} />
+        <Route path="/signIn" Component={Login} />
+        <Route path="/user" Component={UrlShortner} />
+        <Route path="/password-reset" Component={ForgotPassword}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/UrlShortner.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function UrlShortner() {
+  const [link, setlink] = useState("");
+  const [shortLink, setShortLink] = useState("");
+
+  function handleSubmit() {
+    fetch("https://urlshortner-bqh3.onrender.com/shortUrls", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        url: link,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setShortLink(data.data);
+        alert(data.status);
+      });
+  }
+  return (
+    <div className="container">
+      <h1 className="text-center p-5 text-danger">URL - SHORTNER</h1>
+      <div className="d-flex justify-content-center align-items-center">
+        <input
+          className="form-control form-control-lg input"
+          type="text"
+          onChange={(e) => setlink(e.target.value)}
+        />
+      </div>
+      <div className="pt-5 mt-5 d-flex justify-content-center align-items-center">
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          Shrink
+        </button>
+      </div>
+      <div className="text-center pt-5 mt-5">
+        <a className="h3" href={link} target="_blank" rel="noreferrer">
+          {shortLink}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default UrlShortner;
